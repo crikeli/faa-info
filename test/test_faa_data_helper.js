302cabd54964a6b455a7215611fe8a7714780d33
Kelin Christi <christi.kelin20@gmail.com>
2016-05-15 21:11:32 -0700

    Set up basic folder structure

---
+++ test/test_faa_data_helper.js
@@ -0,0 +1,10 @@
+'use strict';
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+var FAADataHelper = require('../faa_data_helper');
+
+describe('FAADataHelper', function() {
+    var subject = new FAADataHelper();
+});
